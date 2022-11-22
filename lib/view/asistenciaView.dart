697a0202f56bc28c1899fb49c61e292c89c73168
NataliRondo <51837916+NataliRondo@users.noreply.github.com>
2022-11-22 10:02:33 -0500

    Update asistenciaView.dart

--- lib/view/asistenciaView.dart
+++ lib/view/asistenciaView.dart
@@ -108,5 +108,4 @@ class _AsistenciaViewState extends State<AsistenciaView> {
 extension StringCasingExtension on String {
   String toCapitalize() =>
       length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
-  // String yourStringModifyingMethod() => write your logic here to modify the string as per your need;
 }
