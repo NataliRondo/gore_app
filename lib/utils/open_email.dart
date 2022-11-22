5a7d86dc3dda0ab4b7bbe2c7d6545cdb0981c938
NataliRondo <51837916+NataliRondo@users.noreply.github.com>
2022-11-22 12:49:21 -0500

    ok

--- lib/utils/open_email.dart
+++ lib/utils/open_email.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:url_launcher/url_launcher_string.dart';
+
 
 //void sendEmail(String email) => launchUrlString("mailto:$email");
 void showNoMailAppsDialog(BuildContext context) {
